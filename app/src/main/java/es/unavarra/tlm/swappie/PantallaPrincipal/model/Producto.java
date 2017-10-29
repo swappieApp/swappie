1844968527cc44921f023ccab9f303ae68765679
enriqueserr9119 <enriqueserr9119@gmail.com>
2017-10-29 13:18:59 +0100

    Se ha añadido la pantalla principal de la App y la clase de las peticiones REST.

---
+++ app/src/main/java/es/unavarra/tlm/swappie/PantallaPrincipal/model/Producto.java
@@ -0,0 +1,59 @@
+package es.unavarra.tlm.swappie.PantallaPrincipal.model;
+
+import android.widget.ImageView;
+
+public class Producto {
+
+    private int productId;
+    private int drawableId;
+    private ImageView image;
+    private String description;
+    private String location;
+
+    public Producto(int drawableId, String description, String location) {
+        this.drawableId = drawableId;
+        this.description = description;
+        this.location = location;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public int getDrawableId() {
+        return drawableId;
+    }
+
+    public void setDrawableId(int drawableId) {
+        this.drawableId = drawableId;
+    }
+
+    public ImageView getImage() {
+        return image;
+    }
+
+    public void setImage(ImageView image) {
+        this.image = image;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
+
